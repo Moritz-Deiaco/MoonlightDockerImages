553bb66f6d497155ecadef59fb43ac0439cb2dee
Moritz <101179677+Moritz-Deiaco@users.noreply.github.com>
2024-04-19 18:13:04 +0200

    added python 3.10 to test if it works, fixed java workflow

---
+++ python/entrypoint.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+cd /home/container
+
+# Make internal docker ip address available to processes.
+TZ=${TZ:-UTC}
+export TZ
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+echo "Python Version:"
+python --version
+
+# replace startup variables
+PARSED=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+
+# Run the server
+eval ${PARSED}
